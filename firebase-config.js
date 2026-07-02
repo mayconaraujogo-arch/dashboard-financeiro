02bc4f812710d9e596fc9616cd4c4852ef8b161e
mayconaraujogo-arch <mayconaraujogo@gmail.com>
2026-07-01 22:08:07 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,12 +7,13 @@
 // Este app usa um documento fixo para sua sincronização pessoal.
 
 export const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyAFHmM_JxBP9Ku1AlxJDk_1LYu_DefVrww",
+  authDomain: "financas-maycon.firebaseapp.com",
+  projectId: "financas-maycon",
+  storageBucket: "financas-maycon.firebasestorage.app",
+  messagingSenderId: "536313767492",
+  appId: "1:536313767492:web:9a05c8d7d951b59a693e11",
+  measurementId: "G-S0CZBQ2DT5"
 };
 
 export const CLOUD_DOC_ID = "maycon-financas";
